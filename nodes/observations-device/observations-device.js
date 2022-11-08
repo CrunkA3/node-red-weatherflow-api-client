d347dec4f50d4dacc80751f87432d67ef0cb39f4
Michael Rohrhirsch <michael.rohrhirsch.dev@outlook.de>
2022-11-08 16:18:57 +0100

    add observations device

---
+++ nodes/observations-device/observations-device.js
@@ -0,0 +1,61 @@
+const { stat } = require('fs');
+
+module.exports = function (RED) {
+    var http = require('http');
+
+    function WeatherFlowNode(config) {
+        RED.nodes.createNode(this, config);
+        var node = this;
+
+        node.client = RED.nodes.getNode(config.client);
+        node.device = config.device;
+        node.deviceType = config.deviceType;
+
+
+        node.on('input', function (msg, send, done) {
+            var input = this;
+            var deviceId = RED.util.evaluateNodeProperty(node.device, node.deviceType, node, msg);
+
+            var query = '?token=' + node.client.token;
+
+            var options = {
+                host: 'swd.weatherflow.com',
+                path: '/swd/rest/observations/device/' + deviceId + query
+            };
+
+            callback = function (response) {
+                var str = '';
+
+                response.on('data', function (chunk) {
+                    str += chunk;
+                });
+
+                response.on('end', function () {
+                    msg.payload = JSON.parse(str);
+
+                    if (msg.payload.status.status_code == 0) {
+                        send(msg);
+                        done();
+                    }
+                    else {
+                        done(msg);
+                    }
+                });
+            }
+
+            try {
+                http
+                    .request(options, callback)
+                    .on("error", (e) => {
+                        done('problem with request: ${e.message}');
+                    })
+                    .end();
+            } catch (error) {
+                done(error);
+            }
+        });
+    }
+
+
+    RED.nodes.registerType("observations-device", WeatherFlowNode);
+}
